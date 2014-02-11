b016cc81b2833168ba41bcae9cba79bf2e428a7e
Joel Johnson <johnson.joel.b@gmail.com>
2014-02-11 12:48:41 -0600

    Failing tests for embedding polymorphic association
    Will need to implement mechanism(s) to allow binary message to specify
    what message type it has encoded into it.

--- spec/embedded_message_spec.rb
+++ spec/embedded_message_spec.rb
@@ -39,4 +39,40 @@ describe "Embedded Messages" do
     end
   end
 
+  describe "with polymorphic relationship" do
+    let(:message) do
+      EmbeddedMessagePolymorphicType.create(:my_message => Uint16Table.create(:field => 5))  
+    end
+    it "creates instance" do
+      message.should be_an_instance_of(EmbeddedMessagePolymorphicType)
+    end
+
+    it "creates associated record" do
+      message.my_message.should be_an_instance_of(Uint16Table)
+    end
+      
+    it "stores assocation type" do
+      message.my_message_type.should == Uint16Table.name
+    end
+
+    it "varies type for association" do
+      message.my_message = Uint8Table.create(:field => 7)
+      message.my_message.should be_an_instance_of(Uint8Table)
+      message.my_message_type.should == Uint8Table.name
+    end
+
+    it "validates embedded message" do
+      message.should be_valid
+    end
+
+    it "writes a binary string" do
+      message.to_binary_s.should_not be_empty
+    end
+
+    it "parses from a binary string" do
+      msg = EmbeddedPolymorphicType.read(message.to_binary_s)
+      msg.should be_valid
+      msg.my_message.field.should == 7
+    end
+  end
 end
--- spec/models/embedded_message_polymorphic_type.rb
+++ spec/models/embedded_message_polymorphic_type.rb
@@ -2,5 +2,5 @@ class EmbeddedMessagePolymorphicType < ActiveRecord::Base
   acts_as_serializable
   endian :big
 
-  embedded_message :my_message, :polymorphic => lambda { |record| record.embedded_message_type }
+  embeds_message :my_message, :polymorphic => lambda { |record| record.embedded_message_type }
 end
